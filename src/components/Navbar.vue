0899e15989c860e0b033f59ec7dfd987ad080fca
MarcinVesqum <marcin.pelszyk90@gmail.com>
2023-01-30 09:30:06 +0100

    added signin page & integration api

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,7 +7,7 @@
         <span class="self-center text-xl font-semibold whitespace-nowrap">Simple E-commerce</span>
     </a>
     <div class="flex order-3">
-      <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="click" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" type="button">Register/login <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+      <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="click" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" type="button">Accounts<svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
       <!-- Dropdown menu -->
       <div id="dropdownHover" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
           <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownHoverButton">
--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,11 +1,64 @@
 <template>
-  <div>
+  <div class="container md:w-1/2 px-5 py-24 mx-auto">
+    <div class="bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+        <h2 class="text-gray-900 text-lg mb-2 title-font text-center font-extrabold uppercase">SingUp</h2>
+        <p class="leading-relaxed mb-5 text-gray-600"></p>
+        <div class="relative mb-4">
+            <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+            <input v-model="userData.email" type="email" id="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+        </div>
+        <div class="relative mb-4">
+            <label for="password" class="leading-7 text-sm text-gray-600">Password</label>
+            <input v-model="userData.password" type="password" id="password" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+        </div>
 
-  </div>
+        <button @click.prevent="signin" class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Signup</button>
+    </div>
+</div>
 </template>
 
 <script setup>
+import { reactive, inject } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
 
+const swal = inject('$swal')
+
+const router = useRouter()
+
+const props = defineProps({
+    baseURL: String
+})
+
+const userData = reactive({
+    email: '',
+    password: ''
+})
+
+const signin = async () => {
+  let body = {
+    email: userData.email,
+    password: userData.password
+  }
+  await axios.post(`${props.baseURL}user/signIn`, body)
+  .then(() => {
+    router.replace('/')
+    swal.fire({
+        icon: 'success',
+        text: 'Login successful',
+        showConfirmButton: false,
+        timer: 1500
+    })
+  }).catch((err) => {
+    console.log('error: ' + err.message)
+    swal.fire({
+      icon: 'error',
+      text: 'check your email or password',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  })
+}
 </script>
 
 <style lang="scss" scoped>
